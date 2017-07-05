a635b4b6401b0023f684d3d8c06afbb7cd266014
Otto Vazquez <ottovazquez@protonmail.ch>
2017-07-05 17:03:01 +0100

    Added JENKISN_SWARM_KNOWN_HOSTS opt

--- jenkins-swarm/cmd.sh
+++ jenkins-swarm/cmd.sh
@@ -1,25 +1,42 @@
 #!/bin/sh
 
-JAR=`ls -1 ${JENKINS_HOME}/swarm-client-*.jar | tail -n 1`
+JAR=`ls -1 ${JENKINS_SWARM_HOME}/swarm-client-*.jar | tail -n 1`
 
-if [ -z "$JENKINS_MASTER_URL" ]; then
+if [ -z "$JENKINS_SWARM_MASTER_URL" ]; then
   echo "JENKINS_MASTER_URL is not set, exit now"
   exit 1
 fi
 
-JENKINS_SWARM_OPTS="-fsroot ${JENKINS_HOME} -master ${JENKINS_MASTER_URL}"
+if [ -n "${JENKISN_SWARM_KNOWN_HOSTS}" ]; then
+    SSH_DIR=$HOME/.ssh
 
-if [ -n "$JENKINS_USERNAME" ]; then
-  JENKINS_SWARM_OPTS="${JENKINS_SWARM_OPTS} -username $JENKINS_USERNAME"
+    if [ ! -d ${SSH_DIR} ]; then
+        mkdir ${SSH_DIR}
+    fi
+    if [ -f ${SSH_DIR}/known_hosts ]; then
+        rm ${SSH_DIR}/known_hosts
+    fi
+
+    touch ${SSH_DIR}/known_hosts
+    for i in $(echo ${JENKISN_SWARM_KNOWN_HOSTS} | tr ',' ' '); do
+        ssh-keyscan -H $i >> ${SSH_DIR}/known_hosts
+    done
+    chmod 600 ${SSH_DIR}/known_hosts
 fi
-if [ -n "$JENKINS_PASSWORD" ]; then
-  JENKINS_SWARM_OPTS="${JENKINS_SWARM_OPTS} -password $JENKINS_PASSWORD"
+
+JENKINS_SWARM_OPTS="-fsroot ${JENKINS_SWARM_HOME} -master ${JENKINS_SWARM_MASTER_URL}"
+
+if [ -n "$JENKINS_SWARM_USERNAME" ]; then
+  JENKINS_SWARM_OPTS="${JENKINS_SWARM_OPTS} -username $JENKINS_SWARM_USERNAME"
+fi
+if [ -n "$JENKINS_SWARM_PASSWORD" ]; then
+  JENKINS_SWARM_OPTS="${JENKINS_SWARM_OPTS} -password $JENKINS_SWARM_PASSWORD"
 fi
 if [ -n "$JENKINS_SWARM_EXECUTORS" ]; then
   JENKINS_SWARM_OPTS="${JENKINS_SWARM_OPTS} -executors $JENKINS_SWARM_EXECUTORS"
 fi
 
-if [ "${JENKINS_SWARM_SSL_VERIFICATION}" == "false" ]; then
+if [ "_${JENKINS_SWARM_SSL_VERIFICATION}" == "_false" ]; then
   JENKINS_SWARM_OPTS="${JENKINS_SWARM_OPTS} -disableSslVerification"
 fi
 
